584b3f0a182b23a3ad72904bed7caccb0579525a
Sadykov Arsen <LoCo10291998@gmail.com>
2025-03-28 18:24:24 +0600

    Changed showing toast success

--- chat-frontend/src/features/NewMessage.tsx
+++ chat-frontend/src/features/NewMessage.tsx
@@ -13,8 +13,8 @@ const NewMessage = () => {
     const onCreateNewProduct = async (message: Message) => {
         try {
             await dispatch(createMessage(message));
-            toast.success("Product was successfully created!");
             await dispatch(fetchAllMessages());
+            toast.success("Product was successfully created!");
             navigate('/');
         } catch (e) {
             console.error(e);
